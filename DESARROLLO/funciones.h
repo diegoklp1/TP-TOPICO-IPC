5a9548f530534015a04421dab62518e571bed3f6
diegoklp1 <llanosdiegowk@gmail.com>
2025-10-30 20:20:37 +0000

    Ajustes finales

--- DESARROLLO/funciones.h
+++ DESARROLLO/funciones.h
@@ -1,8 +1,54 @@
 #ifndef FUNCIONES_H_INCLUDED
 #define FUNCIONES_H_INCLUDED
 
-#include "estructuras.c"
 #include <stdbool.h>
+#include <stdio.h>
+#include <ctype.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define MAX_LINEA 512
+#define TAM_INI 100
+#define FACT_INCR 1.5
+#define TODO_OK 1
+#define ERR_ARCHIVO 2
+#define SIN_MEM 3
+
+typedef struct {
+    char codigo[32];
+    char descripcion[100];
+    char clasificador[64];
+    char indice_ipc[21];
+    char variacion_mensual[17];
+    char variacion_interanual[17];
+    char region[10];
+    char periodo[21];   // AAAAMM
+} RegistroIPC;
+
+typedef struct {
+    char fecha[30];
+    char descripcion[128];
+    int periodo;   // AAAAMM
+    double indice_ipc;
+    char region[64];
+    char grupo[32];
+} Clasificacion;
+
+typedef struct {
+    void* vector;
+    size_t tamanioElemento;
+    size_t cantidadElementos;
+    size_t capacidad;
+} Vector;
+
+typedef struct {
+    char periodo[8]; // Formato "YYYY-MM"
+    double indice;
+    double variacionPct;
+    double montoAjustado;
+} FilaTablaAlquiler;
+
 
 // Convención nombre de funciones : primer palabra en minuscula, la siguiente empiezan con mayuscula.
 
@@ -46,7 +92,7 @@ void calcularAjusteAlquiler(const char* nomArchAperturas);
 void leerMostrarTablaBinario(const char*);
 
 
-static bool redimensionarVector(Vector* vec, double factorInc);
+bool redimensionarVector(Vector* vec, double factorInc);
 int vectorCrear(Vector* vector, size_t tamElem);
 void vectorDestruir(Vector* vec);
 int vectorInsertar(Vector* vec, void* elem);
